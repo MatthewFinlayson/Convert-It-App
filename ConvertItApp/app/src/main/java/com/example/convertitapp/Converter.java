bf59e0276354ba3b91ae1a0d8487fe98b544a7b9
Matthew Finlayson <matt@DESKTOP-4VF9R8R.localdomain>
2021-01-26 20:31:24 -0800

    Add entire project

---
+++ ConvertItApp/app/src/main/java/com/example/convertitapp/Converter.java
@@ -0,0 +1,143 @@
+package com.example.convertitapp;
+
+
+import com.digidemic.unitof.UnitOf;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+public class Converter {
+    private UnitOf.Volume unitOfVolume;
+    private UnitOf.Area unitOfArea;
+    private String unit;
+    private Double input;
+    private static DecimalFormat df2 = new DecimalFormat("#.#############");
+
+
+    public Converter() {
+
+    }
+
+    public void setUnit(String text) {
+        this.unit = text;
+
+    }
+
+    public void setInput(Double input) {
+        this.input = input;
+    }
+
+    public ArrayList<String> convertVolume() {
+
+        ArrayList<String> result = new ArrayList<>();
+
+        switch (unit) {
+            case "m3":
+                unitOfVolume = new UnitOf.Volume().fromCubicMeters(input);
+                break;
+            case "km3":
+                unitOfVolume = new UnitOf.Volume().fromCubicKilometers(input);
+                break;
+            case "cm3":
+                unitOfVolume = new UnitOf.Volume().fromCubicCentimeters(input);
+                break;
+            case "mm3":
+                unitOfVolume = new UnitOf.Volume().fromCubicMillimeters(input);
+                break;
+            case "l":
+                unitOfVolume = new UnitOf.Volume().fromLiters(input);
+                break;
+            case "ml":
+                unitOfVolume = new UnitOf.Volume().fromMilliliters(input);
+                break;
+            case "gal (US)":
+                unitOfVolume = new UnitOf.Volume().fromGallonsUS(input);
+                break;
+            case "qt (US)":
+                unitOfVolume = new UnitOf.Volume().fromQuartsUS(input);
+                break;
+            case "pt (US)":
+                unitOfVolume = new UnitOf.Volume().fromPintsUS(input);
+                break;
+            case "fl oz (US)":
+                unitOfVolume = new UnitOf.Volume().fromFluidOuncesUS(input);
+                break;
+            case "tbsp (US)":
+                unitOfVolume = new UnitOf.Volume().fromTablespoonsUS(input);
+                break;
+            case "tsp (US)":
+                unitOfVolume = new UnitOf.Volume().fromTeaspoonsUS(input);
+                break;
+        }
+
+        result.add(df2.format(unitOfVolume.toCubicMeters()));
+        result.add(df2.format(unitOfVolume.toCubicKilometers()));
+        result.add(df2.format(unitOfVolume.toCubicCentimeters()));
+        result.add(df2.format(unitOfVolume.toCubicMillimeters()));
+        result.add(df2.format(unitOfVolume.toLiters()));
+        result.add(df2.format(unitOfVolume.toMilliliters()));
+        result.add(df2.format(unitOfVolume.toGallonsUS()));
+        result.add(df2.format(unitOfVolume.toQuartsUS()));
+        result.add(df2.format(unitOfVolume.toPintsUS()));
+        result.add(df2.format(unitOfVolume.toFluidOuncesUS()));
+        result.add(df2.format(unitOfVolume.toTablespoonsUS()));
+        result.add(df2.format(unitOfVolume.toTeaspoonsUS()));
+
+        return result;
+    }
+
+    public ArrayList<String> convertArea() {
+
+        ArrayList<String> result = new ArrayList<>();
+
+        switch (unit) {
+            case "m2":
+                unitOfArea = new UnitOf.Area().fromSquareMeters(input);
+                break;
+            case "km2":
+                unitOfArea = new UnitOf.Area().fromSquareKilometers(input);
+                break;
+            case "cm2":
+                unitOfArea = new UnitOf.Area().fromSquareCentimeters(input);
+                break;
+            case "mm2":
+                unitOfArea = new UnitOf.Area().fromSquareMillimeters(input);
+                break;
+            case "\u00B52":
+                unitOfArea = new UnitOf.Area().fromSquareMicrometers(input);
+                break;
+            case "mi2":
+                unitOfArea = new UnitOf.Area().fromSquareMiles(input);
+                break;
+            case "yd2":
+                unitOfArea = new UnitOf.Area().fromSquareYards(input);
+                break;
+            case "ft2":
+                unitOfArea = new UnitOf.Area().fromSquareFeet(input);
+                break;
+            case "in2":
+                unitOfArea = new UnitOf.Area().fromSquareInches(input);
+                break;
+            case "ha":
+                unitOfArea = new UnitOf.Area().fromHectares(input);
+                break;
+            case "acre":
+                unitOfArea = new UnitOf.Area().fromAcres(input);
+                break;
+        }
+
+        result.add(df2.format(unitOfArea.toSquareMeters()));
+        result.add(df2.format(unitOfArea.toSquareKilometers()));
+        result.add(df2.format(unitOfArea.toSquareCentimeters()));
+        result.add(df2.format(unitOfArea.toSquareMillimeters()));
+        result.add(df2.format(unitOfArea.toSquareMicrometers()));
+        result.add(df2.format(unitOfArea.toSquareMiles()));
+        result.add(df2.format(unitOfArea.toSquareYards()));
+        result.add(df2.format(unitOfArea.toSquareFeet()));
+        result.add(df2.format(unitOfArea.toSquareInches()));
+        result.add(df2.format(unitOfArea.toHectares()));
+        result.add(df2.format(unitOfArea.toAcres()));
+
+        return result;
+    }
+}
---
+++ ConvertItApp/app/src/main/java/com/example/convertitapp/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.convertitapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+
+        ImageButton massBtn = (ImageButton) findViewById(R.id.massBtn);
+        ImageButton currencyBtn = (ImageButton) findViewById(R.id.currencyBtn);
+        ImageButton tempBtn = (ImageButton) findViewById(R.id.tempBtn);
+        ImageButton lengthBtn = (ImageButton) findViewById(R.id.lenthBtn);
+        ImageButton volumeBtn = (ImageButton) findViewById(R.id.volumBtn);
+        ImageButton areaBtn = (ImageButton) findViewById(R.id.areaBtn);
+
+
+        massBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MassConvert.class));
+            }
+        });
+
+
+        currencyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, CurrencyConvert.class));
+            }
+        });
+
+
+        tempBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, tempConvert.class));
+            }
+        });
+
+
+        lengthBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LengthConvert.class));
+            }
+        });
+
+        volumeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Volume.class));
+            }
+        });
+
+        areaBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, Area.class));
+            }
+        });
+
+
+    }
+}
